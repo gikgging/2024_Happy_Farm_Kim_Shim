d4e11a883118c3251629c4314ade46492c30c37b
ezmilyy31 <tlatjdus0531@naver.com>
2024-12-06 18:24:52 +0100

    Merge remote-tracking branch 'origin/Hajin' into main

--- Happy_Farm_Operator/Happy Farm (Kim and Shim)/Happy Farm (Kim and Shim)/main/ItemFactory.java
+++ Happy_Farm_Operator/Happy Farm (Kim and Shim)/Happy Farm (Kim and Shim)/main/ItemFactory.java
@@ -5,7 +5,10 @@ import items.Egg;
 import items.Namool;
 import items.RedBull;
 
+//ItemFactory give information about the item which we only know the name
 public class ItemFactory {
+	
+	//Give info about the crop
     public static Item createItem(String itemName) {
         switch(itemName) {
             case "Compost":
